32478a0ee6de16e1cada7118be0c3013a58a6b35
mickaelchoidev <mickaelchoidev@gmail.com>
2020-09-24 17:03:43 +0200

    header + logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import Logo from "./components/Logo";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="App__header">
+        <div className="Header__logo">
+          <Logo />
+          <div className="Header__title">Instagram clone</div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="Logo">
+      <div class="Navbar-ball ball1">
+        <div class="Navbar-ball ball2">
+          <div class="Navbar-ball ball3">
+            <div class="Navbar-ball ball4">
+              <div class="Navbar-ball ball5">
+                <div class="Navbar-ball ball6">
+                  <div class="Navbar-ball ball7"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
