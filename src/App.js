5e4cac58e0b52ed8d92feb47bd0433281e8483f9
mickaelchoidev <mickaelchoidev@gmail.com>
2020-09-26 09:13:07 +0200

    modal sign up and sign in + firebase Auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
 import React, { useState, useEffect } from "react";
-import { db } from "./config/Firebase";
+import { db, auth } from "./config/Firebase";
 
 import Logo from "./components/Logo";
+import SignUp from "./auth/SignUp";
+import SignIn from "./auth/SignIn";
 import Post from "./components/Post";
+import Button from "@material-ui/core/Button";
 
 import "./App.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [openSignUp, setOpenSignUp] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-  ///////////////////////******************///////////////////////
+  // const onOpenModal = () => setOpen(true);
+  const onOpenSignUp = () => setOpenSignUp(true);
+  const onOpenSignIn = () => setOpenSignIn(true);
+  const onCloseModal = () => {
+    setOpenSignUp(false);
+    setOpenSignIn(false);
+  };
+  const onchangeUsername = (e) => setUsername(e.target.value);
+  const onchangeEmail = (e) => setEmail(e.target.value);
+  const onchangePassword = (e) => setPassword(e.target.value);
+
+  const onSignUp = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+    setOpenSignUp(false);
+  };
+
+  const onSignIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+    setOpenSignIn(false);
+  };
+
+  const onLogOut = () => auth.signOut();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in ...
+        console.log(authUser);
+        setUser(authUser);
+        // if (authUser.displayName) {
+        //   // don't update username
+        // } else {
+        //   // if we just created a user
+        //   return authUser.updateProfile({
+        //     displayName: username,
+        //   });
+        // }
+      } else {
+        // user has logged out ...
+        setUser(null);
+      }
+    });
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    };
+  }, [user, username]);
 
   // useEffect => runs the code based on a speific conditions
   useEffect(() => {
@@ -25,8 +91,6 @@ function App() {
     });
   }, []);
 
-  ///////////////////////******************///////////////////////
-
   return (
     <div className="App">
       <header className="App__header">
@@ -34,7 +98,58 @@ function App() {
           <Logo />
           <div className="Header__title">Instagram clone</div>
         </div>
+
+        {user ? (
+          <div className="App__logContainer">
+            <Button
+              variant="contained"
+              className="App__logButton"
+              onClick={onLogOut}
+            >
+              LOG OUT
+            </Button>
+          </div>
+        ) : (
+          <div className="App__logContainer">
+            <div className="App__logButton">
+              <Button
+                variant="contained"
+                className="App__logButton"
+                onClick={onOpenSignIn}
+              >
+                SIGN IN
+              </Button>
+            </div>
+            <div className="App__logButton">
+              <Button variant="contained" onClick={onOpenSignUp}>
+                SIGN UP
+              </Button>
+            </div>
+          </div>
+        )}
       </header>
+
+      <SignUp
+        modalState={openSignUp}
+        onCloseModal={onCloseModal}
+        username={username}
+        email={email}
+        password={password}
+        onChangeUsername={onchangeUsername}
+        onChangeEmail={onchangeEmail}
+        onChangePassword={onchangePassword}
+        onSignUp={onSignUp}
+      />
+      <SignIn
+        modalState={openSignIn}
+        onCloseModal={onCloseModal}
+        email={email}
+        password={password}
+        onChangeEmail={onchangeEmail}
+        onChangePassword={onchangePassword}
+        onSignIn={onSignIn}
+      />
+
       {posts.map(({ id, post }) => (
         <Post
           key={id}
---
+++ src/auth/SignUp.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: "#dde3e4",
+    border: "2px solid #3f51b5",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(3, 3, 3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    color: "#08090a",
+    marginBottom: "1rem",
+    width: "60vw",
+  },
+  button: {
+    width: "100%",
+  },
+}));
+
+function SignUp(props) {
+  const classes = useStyles();
+  const {
+    modalState,
+    onCloseModal,
+    username,
+    email,
+    password,
+    onChangeUsername,
+    onChangeEmail,
+    onChangePassword,
+    onSignUp,
+  } = props;
+
+  return (
+    <div className="Modal__signup">
+      <Modal
+        className={classes.modal}
+        open={modalState}
+        onClose={onCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={modalState}>
+          <form className={classes.paper} onSubmit={onSignUp}>
+            <TextField
+              id="outlined-basic"
+              className={classes.input}
+              label="Username"
+              variant="outlined"
+              value={username}
+              onChange={onChangeUsername}
+            />
+            <TextField
+              type="email"
+              id="outlined-basic"
+              className={classes.input}
+              label="email"
+              variant="outlined"
+              value={email}
+              onChange={onChangeEmail}
+            />
+            <TextField
+              required
+              type="password"
+              id="outlined-basic"
+              className={classes.input}
+              label="Password"
+              variant="outlined"
+              value={password}
+              onChange={onChangePassword}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              SIGN UP
+            </Button>
+          </form>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+export default SignUp;
